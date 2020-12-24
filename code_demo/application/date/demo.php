1f6a4bffcfb59313821f52079807f5693ecb21d7
zhudechao <1474083407@qq.com>
2020-12-24 15:17:05 +0800

    laravel orm

--- code_demo/application/date/demo.php
+++ code_demo/application/date/demo.php
@@ -17,10 +17,27 @@ class demo
 {
     public function main(){
         $this->day();
+//        $this->month();
     }
 
     private function day(){
         //获取当前年月日 2020-12-22
         echo Carbon::now()->toDateString();
+        echo "</br>";
+
+        $dt = Carbon::parse("2020-12-22 11:43:00");
+        //减三个工作日(会把周六周日排除)
+        echo $dt->subWeekday(3);
+        echo "</br>";
+
+        //日期减一天
+        echo "</br>日期减一天:2020-11-30 to ";
+        $dt = Carbon::parse("2020-11-30");
+        echo $dt->subDay();
+        echo "</br>";
+    }
+
+    private function month(){
+
     }
 }
